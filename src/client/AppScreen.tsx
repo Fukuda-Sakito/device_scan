652804289b5a60725de30e91629ca72fcc916d32
Fukuda-Sakito <fkdsakito@gmail.com>
2024-01-14 15:03:18 +0900

    初回読み込み時のCSSアニメーションを調整

--- src/client/AppScreen.tsx
+++ src/client/AppScreen.tsx
@@ -32,9 +32,9 @@ const AppScreen = () => {
         <div className="bg-white bg-opacity-20 rounded-full p-1 inline-block">
           {/* <img src="./images/NETWORK_NBG.png" alt="Computer Monitor" className="w-64 h-64 object-cover rounded-full" /> */}
         </div>
-        <h1 className="text-white text-4xl font-bold mt-4">Device Scanner へようこそ</h1>
-        <p className="text-white text-opacity-70 mt-2">ネットワークの状態確認しましょう。</p>
-        <button onClick={startScan} className="mt-4 bg-white text-purple-600 font-semibold py-2 px-4 rounded-full hover:bg-purple-100">
+        <h1 style={{ animation: 'fadeInUp 1s ease-in 1s forwards' }} className="text-white text-4xl font-bold mt-4">Device Scanner へようこそ</h1>
+        <p style={{ animation: 'fadeInUp 1s ease-in 1.5s forwards' }} className="text-white text-opacity-70 mt-2">ネットワークの状態確認しましょう。</p>
+        <button style={{ animation: 'fadeInUp 1s ease-in 2s forwards' }} onClick={startScan} className="mt-4 bg-white text-purple-600 font-semibold py-2 px-4 rounded-full hover:bg-purple-100">
           スタート
         </button>
       </div>
