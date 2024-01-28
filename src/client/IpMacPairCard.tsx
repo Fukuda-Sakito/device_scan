b3f76b72942846becbaacf5703d90fa44e1d769d
Fukuda-Sakito <fkdsakito@gmail.com>
2024-01-29 01:28:54 +0900

    カードの中身を中央揃えにし、> を追加

--- src/client/IpMacPairCard.tsx
+++ src/client/IpMacPairCard.tsx
@@ -16,12 +16,13 @@ const IpMacPairCard: React.FC<IpMacPairCardProps> = ({ vendor, os }) => {
       timeout={300}
       classNames="fade"
     >
-      <div className="m-2 w-1/6 p-4 bg-gradient-to-r from-indigo-700 to-sky-700 rounded-lg shadow-lg flex items-center space-x-2">
+      <div className="m-2 w-1/6 p-4 bg-gradient-to-r from-indigo-700 to-sky-700 rounded-lg shadow-lg flex items-center justify-center space-x-2 cursor-pointer">
         <img src={logo} alt="logo" style={{ width: '20px', marginRight: '10px', alignSelf: 'center' }} />
         <div className="text-white">
           <p className="text-center">Vendor: {vendor}</p>
           <p className="text-center">OS: {os}</p>
         </div>
+        <div className="text-white text-2xl">{'>'}</div>
       </div>
     </CSSTransition>
   );
